c08ce2c741d35b0d4f1d2dcd15570108a35d72be
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-30 12:53:18 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 var isNullOrUndefined = require("is_null_or_undefined");
 
 
-module.exports = function isPrimitive(obj) {
+module.exports = isPrimitive;
+
+
+function isPrimitive(obj) {
     var typeStr;
     return isNullOrUndefined(obj) || ((typeStr = typeof(obj)) !== "object" && typeStr !== "function") || false;
-};
+}
